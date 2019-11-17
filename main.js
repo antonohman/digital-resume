f76f2843d2541aa12de883c58934498028d04e58
Anton Öhman <anton.ohman@hotmail.com>
2019-11-17 17:36:37 +0100

    Changed headeranimation

---
+++ main.js
@@ -0,0 +1,5 @@
+/* const headerText = document.querySelectorAll("#headerText path");
+
+for (let i = 0; i<headerText.length; i++) {
+    console.log(`Letter ${i} is ${headerText[i].getTotalLength()}`);
+} */
