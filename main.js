e27528ef94e7099e5afdaf04fb7951c9e85bf405
Anton Öhman <anton.ohman@hotmail.com>
2019-11-18 17:59:04 +0100

    Added animation to all logos

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-/* const logoBorder = document.querySelectorAll(".logoBorder rect");
+
+/* FIND NUMBER FOR stroke-dasharray AND stroke-dashoffset */
+
+/* const logoBorder = document.querySelectorAll(".sonyvegasLogoBorder rect");
 
 for (let i = 0; i<logoBorder.length; i++) {
     console.log(`Letter ${i} is ${logoBorder[i].getTotalLength()}`);
