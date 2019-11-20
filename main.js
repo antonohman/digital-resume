0c1f2fbf4537c095763468e1ade8f6bab4f5e5f7
Anton Öhman <anton.ohman@hotmail.com>
2019-11-20 13:59:30 +0100

    Added clock

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 
-/* FIND NUMBER FOR stroke-dasharray AND stroke-dashoffset */
+// FIND NUMBER FOR stroke-dasharray AND stroke-dashoffset */
 
 /* const logoBorder = document.querySelectorAll(".sonyvegasLogoBorder rect");
 
 for (let i = 0; i<logoBorder.length; i++) {
     console.log(`Letter ${i} is ${logoBorder[i].getTotalLength()}`);
 } */
+
+// BACK TO THE TOP-BUTTON
 
 let mybutton = document.getElementById("topButton");
 
@@ -22,4 +24,22 @@ function scrollFunction() {
 function backToTop() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
+}
+
+// CLOCK
+
+function startTime() {
+  let today = new Date();
+  let h = today.getHours();
+  let m = today.getMinutes();
+  let s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById("clock").innerHTML =
+  h + ":" + m + ":" + s;
+  let t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+  if (i < 10) {i = "0" + i};
+  return i;
 }
